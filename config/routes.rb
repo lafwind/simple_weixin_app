b6150009800c9ffab30e10672d3318f35a3ded5d
Lafwind <lafwind@gmail.com>
2015-05-12 23:05:52 +0800

    添加weixin_rails_middleware的gem，可实现回复

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount WeixinRailsMiddleware::Engine, at: "/"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
